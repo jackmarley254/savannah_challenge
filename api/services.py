794f20b479f46760d4d5798cbf4a4352a5895891
jackmarley254 <engndiritu1@gmail.com>
2025-09-29 17:07:21 +0300

    feat(sms): Implement Africa's Talking SMS integration

--- api/services.py
+++ api/services.py
@@ -1,6 +1,6 @@
 import os
-#import africastalking
-from africastalking.AfricasTalkingGateway import AfricasTalkingGateway
+import africastalking
+#from africastalking.AfricasTalkingGateway import AfricasTalkingGateway
 from django.conf import settings
 
 def send_order_alert_sms(customer_phone_number, message): 
@@ -9,9 +9,11 @@ def send_order_alert_sms(customer_phone_number, message):
     """
     username = settings.AT_USERNAME
     api_key = settings.AT_API_KEY
-    #africastalking.initialize(username, api_key)
     
-    gateway = AfricasTalkingGateway(username, api_key)
+    africastalking.initialize(username, api_key)
+    sms = africastalking.SMS
+    
+    #gateway = AfricasTalkingGateway(username, api_key)
     
     recipients = [customer_phone_number]
 
@@ -19,7 +21,7 @@ def send_order_alert_sms(customer_phone_number, message):
     #sms = africastalking.SMS
     
     try:
-        response = gateway.sendMessage(recipients, message)
+        response = sms.send(recipients, message)
         print(f"SMS response: {response}")
         return response
     except Exception as e:
